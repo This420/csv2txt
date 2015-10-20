417fc2187c988181899de2f008961781eb33e01e
Koji Matsuda <matsuda@accs-c.co.jp>
2015-10-20 09:03:51 +0900

    いったん完成

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/transform"
-	"encoding/csv"
-	"log"
-	"flag"
-	"os"
 	"io"
-	"io/ioutil"
-	"net/smtp"
+	"log"
+	"os"
+	"bufio"
 )
 
 func main() {
+	var writer *bufio.Writer
+
+	write_file, _ := os.OpenFile("out.txt", os.O_WRONLY|os.O_CREATE, 0600)
+	writer = bufio.NewWriter(write_file)
+
 	flag.Parse()
 
 	file, err := os.Open(flag.Arg(0))
@@ -20,6 +24,8 @@ func main() {
 	defer file.Close()
 
 	reader := csv.NewReader(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
+
+	var cnt = 0
 
 	for {
 		record, err := reader.Read()
@@ -29,29 +35,29 @@ func main() {
 			failOnError(err)
 		}
 
+		cnt++
+		if cnt == 1 { continue }
+
 		log.Printf("%#v", record)
-		content := []byte(record[0] + "\n" + record[1] + "\n")
-		ioutil.WriteFile("out.txt", content, os.ModePerm)
+
+		var contact string
+
+		if record[1] == "" {
+			contact = "一般"
+		} else {
+			contact = record[1]
+		}
+
+		content := []byte("----------------------------------------------------------------------------------------------------------\n" +
+			"●問い合わせ元\n" +
+			contact + "\n" +
+			"●質問\n" +
+			record[7] + "\n" +
+			"★回答\n" +
+			record[11] + "\n")
+		writer.Write(content)
+		writer.Flush()
 	}
-
-	auth := smtp.PlainAuth(
-		"",
-		"", // 送信元メアド
-		"", // パスワード
-		"smtp.gmail.com",
-	)
-
-	err2 := smtp.SendMail(
-		"smtp.gmail.com:587",
-		auth,
-		"", // 送信元メアド
-		[]string{""},　// 送信先メアド
-		[]byte("メール本文"), // 本文
-	)
-
-	if err2 != nil {
-		log.Fatal(err2)
-	}	
 }
 
 func failOnError(err error) {
